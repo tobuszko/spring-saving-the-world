511cdbdd85162582a084a0af0a591cc0c9a0d4e8
Mateusz Wręczycki <setsudan.hana@gmail.com>
2019-03-01 23:19:24 +0100

    first commit

---
+++ src/main/java/pl/edu/wszib/savingtheworld/SavingTheWorldApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.savingtheworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavingTheWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavingTheWorldApplication.class, args);
+	}
+
+}
